08fd3ef01ee87e96ef2b10190f5e53e7c65f25dc
Yevhenii Hyzyla <hyzyla@gmail.com>
2025-04-23 21:43:07 +0200

    🤝 Update selecting default configuration

--- src/common/askers.ts
+++ src/common/askers.ts
@@ -1,7 +1,8 @@
 import { type XcodeConfiguration, getBuildConfigurations } from "./cli/scripts";
 import { showQuickPick } from "./quick-pick";
 
-export const DEFAULT_CONFIGURATION = "Debug";
+export const DEFAULT_DEBUG_CONFIGURATION = "Debug";
+export const DEFAULT_RELEASE_CONFIGURATION = "Release";
 
 /**
  * Base function to ask user to select configuration it doesn't use cache nor store selected configuration
@@ -17,14 +18,31 @@ export async function askConfigurationBase(options: {
   });
 
   // Use default configuration if no configurations found
+  // todo: we can try to parse configurations from schemes files or ask user to enter configuration name manually
   if (configurations.length === 0) {
-    return DEFAULT_CONFIGURATION;
+    return DEFAULT_DEBUG_CONFIGURATION;
   }
 
-  // Use default configuration if it exists
-  if (configurations.some((configuration) => configuration.name === DEFAULT_CONFIGURATION)) {
-    return DEFAULT_CONFIGURATION;
+  // When we have only one configuration let's use it as default
+  if (configurations.length === 1) {
+    return configurations[0].name;
   }
+
+  // This is most common case. When new project is created by default Xcode creates Debug and Release configurations
+  // and "Debug" is mostly set as default configuration for launching application. To avoid confusion we will just use
+  // "Debug" configuration as default and don't ask user to select configuration.
+  if (
+    configurations.length === 2 &&
+    configurations.some((c) => c.name === DEFAULT_DEBUG_CONFIGURATION) &&
+    configurations.some((c) => c.name === DEFAULT_RELEASE_CONFIGURATION)
+  ) {
+    return DEFAULT_DEBUG_CONFIGURATION;
+  }
+
+  // todo: we can try to find configuration in schemes files for "launch" action
+  // File: MyApp.xcodeproj/xcuserdata/YourName.xcuserdatad/xcschemes/MyApp.xcscheme
+  // Path: /Scheme/BuildAction[buildConfiguration]
+  // Path: /Scheme/LaunchAction[buildConfiguration]
 
   // Give user a choice to select configuration if we don't know wich one to use
   return await showConfigurationPicker(configurations);
