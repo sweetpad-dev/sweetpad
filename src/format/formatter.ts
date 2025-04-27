6b3273b7ed69a980fa957fc035f6178db69d252b
Yevhenii Hyzyla <hyzyla@gmail.com>
2025-04-27 21:17:26 +0200

    🚭 Use swift-format from xcode by default
    #136

--- src/format/formatter.ts
+++ src/format/formatter.ts
@@ -1,69 +1,119 @@
-import type * as vscode from "vscode";
+import * as vscode from "vscode";
+import { getXcodeVersionInstalled } from "../common/cli/scripts.js";
 import { getWorkspaceConfig } from "../common/config.js";
 import { exec } from "../common/exec.js";
 import { Timer } from "../common/timer.js";
 import { formatLogger } from "./logger.js";
 
 /**
- * Get formatter command parameters from workspace configuration.
+ * Register swiftpad as formatter for Swift documents. User then can use
+ * "editor.defaultFormatter" in `.vscode/settings.json` to set swiftpad as default formatter
+ * for Swift documents.
  */
-function getFormatterCommand(filename: string): {
-  command: string;
-  args: string[];
-} {
-  const path = getWorkspaceConfig("format.path");
-
-  // We use "swift-format" as default command if no path is provided,
-  // "args" config are ignored in this case
-  if (!path) {
-    return {
-      command: "swift-format",
-      args: ["--in-place", filename],
-    };
-  }
-
-  // By default we use "swift-format" arguments
-  const args: string[] | undefined = getWorkspaceConfig("format.args") ?? ["--in-place", "${file}"];
-  const replacedArgs = args.map((arg) => (arg === "${file}" ? filename : arg));
-
-  return {
-    command: path,
-    args: replacedArgs,
-  };
+export function registerFormatProvider(formatter: SwiftFormattingProvider): vscode.Disposable {
+  return vscode.languages.registerDocumentFormattingEditProvider("swift", formatter);
 }
 
-/**
- * Format given document using swift-format executable.
- */
-export async function formatDocument(document: vscode.TextDocument) {
-  if (document.languageId !== "swift") {
-    return;
+export class SwiftFormattingProvider implements vscode.DocumentFormattingEditProvider {
+  private isBundledSwiftFormat: boolean | undefined = undefined;
+
+  provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+    // if I await this function, vscode won't update the document. i have no idea why :/
+    void this.formatDocument(document);
+    // we edit directly in the file, so no edits are returned
+    return [];
   }
 
-  const filename = document.fileName;
-  const { command, args } = getFormatterCommand(filename);
+  /**
+   * Checks if swift-format is bundled with Xcode (available since Xcode 16).
+   */
+  async isSwiftFormatXcodeBundled(): Promise<boolean> {
+    if (this.isBundledSwiftFormat !== undefined) {
+      return this.isBundledSwiftFormat;
+    }
 
-  const timer = new Timer();
-  try {
-    await exec({
-      command: command,
-      args: args,
-    });
-  } catch (error) {
-    formatLogger.error("Failed to format code", {
+    try {
+      const xcodeVersion = await getXcodeVersionInstalled();
+      if (xcodeVersion.major >= 16) {
+        await exec({ command: "xcrun", args: ["--find", "swift-format"] });
+        this.isBundledSwiftFormat = true;
+        return true;
+      }
+    } catch (error) {
+      formatLogger.debug("Swift-format not available", { error });
+    }
+
+    this.isBundledSwiftFormat = false;
+    return false;
+  }
+
+  /**
+   * Get formatter command parameters from workspace configuration.
+   */
+  async getFormatterCommand(filename: string): Promise<{
+    command: string;
+    args: string[];
+  }> {
+    // User might specify a custom arguments and path for swift-format in the workspace settings.
+    const rawArgs = getWorkspaceConfig("format.args");
+    let args: string[];
+    if (rawArgs && Array.isArray(rawArgs)) {
+      // For user supplied arguments, we use "${file}" as a placeholder for the file name
+      args = rawArgs.map((arg) => (arg === "${file}" ? filename : arg));
+    } else {
+      // This is default parameters for swift-format. For different formatter with different parameters,
+      // user should specify format.args in the workspace settings.
+      args = ["--in-place", filename];
+    }
+
+    const path = getWorkspaceConfig("format.path");
+    if (path) {
+      return { command: path, args: args };
+    }
+
+    // Since Xcode 16, swift-format is bundled with Xcode. By default, we try to use it.
+    // WIKI: xcrun is cli tool that is used to run Xcode tools bundled with Xcode
+    if (await this.isSwiftFormatXcodeBundled()) {
+      return { command: "xcrun", args: ["swift-format", ...args] };
+    }
+
+    // swift-format can be also installed via Homebrew for older Xcode versions
+    return { command: "swift-format", args: [...args] };
+  }
+
+  /**
+   * Format given document using swift-format executable.
+   */
+  async formatDocument(document: vscode.TextDocument) {
+    if (document.languageId !== "swift") {
+      return;
+    }
+
+    const filename = document.fileName;
+    const { command, args } = await this.getFormatterCommand(filename);
+
+    const timer = new Timer();
+    try {
+      await exec({
+        command: command,
+        args: args,
+      });
+    } catch (error) {
+      formatLogger.error("Failed to format code", {
+        executable: command,
+        args: args,
+        filename: filename,
+        execTime: `${timer.elapsed}ms`,
+        error: error,
+      });
+      return;
+    }
+
+    formatLogger.log("Code successfully formatted", {
       executable: command,
       args: args,
       filename: filename,
       execTime: `${timer.elapsed}ms`,
-      error: error,
     });
-    return;
   }
-
-  formatLogger.log("Code successfully formatted", {
-    executable: command,
-    args: args,
-    filename: filename,
-    execTime: `${timer.elapsed}ms`,
-  });
 }
