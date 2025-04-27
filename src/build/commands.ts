c36c7671c3053549023377db2e6a84d73a434e7b
Daniel Bernal <danielbernal@hey.com>
2025-04-28 01:49:19 +0200

    Added more performance flags

--- src/build/commands.ts
+++ src/build/commands.ts
@@ -496,6 +496,8 @@ export async function buildApp(
   if (options.debug) {
     command.addBuildSettings("GCC_GENERATE_DEBUGGING_SYMBOLS", "YES");
     command.addBuildSettings("ONLY_ACTIVE_ARCH", "YES");
+    command.addBuildSettings("-UseModernBuildSystem", "YES");
+    command.addBuildSettings("COMPILER_INDEX_STORE_ENABLE", "NO");    
   }
   
   command.addParameters("-scheme", options.scheme);
@@ -509,6 +511,10 @@ export async function buildApp(
   if (allowProvisioningUpdates) {
     command.addOption("-allowProvisioningUpdates");
   }
+  
+  // Skip macro validation to improve performance.
+  command.addOption("-skipMacroValidation");
+  
   if (options.shouldClean) {
     command.addAction("clean");
   }
