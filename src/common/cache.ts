1e5ac4658acca705aa4e731c9a7d5414703d0feb
Yevhenii Hyzyla <hyzyla@gmail.com>
2025-05-31 16:53:36 +0200

    updates

--- src/common/cache.ts
+++ src/common/cache.ts
@@ -1,5 +1,5 @@
 /**
- * Caches the result of a function call based on its arguments.
+ * Caches the result of a function call based on its arguments
  */
 export function cache<T, Args extends unknown[]>(
   fn: (...args: Args) => Promise<T>,
@@ -7,25 +7,18 @@ export function cache<T, Args extends unknown[]>(
   (...args: Args): Promise<T>;
   clearCache: () => void;
 } {
-  const cache: Record<string, T> = {};
+  const cache = new Map<string, T>();
 
   const cachedFn = async (...args: Args): Promise<T> => {
     const key = JSON.stringify(args);
-    if (key in cache) {
-      return cache[key];
+    if (cache.has(key)) {
+      return cache.get(key) as T;
     }
     const result = await fn(...args);
-    cache[key] = result;
+    cache.set(key, result);
     return result;
   };
 
-  cachedFn.clearCache = () => {
-    for (const key in cache) {
-      if (Object.prototype.hasOwnProperty.call(cache, key)) {
-        delete cache[key];
-      }
-    }
-  };
-
+  cachedFn.clearCache = () => cache.clear();
   return cachedFn;
 }
